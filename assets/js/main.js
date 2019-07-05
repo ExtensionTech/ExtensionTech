ac29da404fbfc64327487feb89531f04d733f0ed
Lachlan Gilmour <lachlan@extension.technology>
2019-07-05 23:19:01 +1000

    better desktop menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,15 @@ $(".dropdown-nav").click (function(){
 	$(".dropdown-nav").toggleClass("open");
 	$("#dropdown-main").toggleClass("open");
 });
+$('#dropdown-main').on('touchstart', function (e) {
+    'use strict'; //satisfy code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('hover')) {
+        return true;
+    } else {
+        link.addClass('hover');
+        $('#dropdown-main').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+    }
+});

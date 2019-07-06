591d2a2ef330435b9e86b98e3529f090a4eb1d04
Lachlan Gilmour <lachlan@extension.technology>
2019-07-06 13:03:25 +1000

    better ALLLL menu

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -5,8 +5,10 @@ $(".navToggle").click (function(){
 	$(this).toggleClass("open");
 	$("nav").toggleClass("open");
 });
-$(".dropdown-nav").click (function(e){
+$(".dropdown-nav").on('touchstart', function (e) {
 	if ($(".dropdown-nav").hasClass('open')) {
+		$(".dropdown-nav").toggleClass("open");
+		$("#dropdown-main").toggleClass("open");
 		return true;
 	} else {
 		e.preventDefault();

0eaa5bd98460750003a2bf6450bcb285ec2a7e52
Lachlan Gilmour <lachlan@extension.technology>
2019-07-06 15:08:58 +1000

    better home page

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -10,17 +10,3 @@ $(".dropdown-nav").click (function(){
 	$("#dropdown-main").toggleClass("open");
 	$(".arrow").toggleClass("open");
 });
-/*
-$(".dropdown-nav").on('touchstart', function (e) {
-	if ($(".dropdown-nav").hasClass('open')) {
-		$(".dropdown-nav").toggleClass("open");
-		$("#dropdown-main").toggleClass("open");
-		return true;
-	} else {
-		e.preventDefault();
-	}
-	$(".dropdown-nav").toggleClass("open");
-	$("#dropdown-main").toggleClass("open");
-	return false;
-});
-*/

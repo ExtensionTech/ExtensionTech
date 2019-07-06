335c9a14f1f35101ac5f08011e7ae950ac6d8d33
Lachlan Gilmour <lachlan@extension.technology>
2019-07-06 12:48:10 +1000

    better ALLLL menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,13 @@ $(".navToggle").click (function(){
 	$(this).toggleClass("open");
 	$("nav").toggleClass("open");
 });
-$(".dropdown-nav").click (function(){
+$(".dropdown-nav").click (function(e){
+	if ($(".dropdown-nav").hasClass('open')) {
+		return true;
+	} else {
+		e.preventDefault();
+	}
 	$(".dropdown-nav").toggleClass("open");
 	$("#dropdown-main").toggleClass("open");
-});
-$('#dropdown-main').on('touchstart', function (e) {
-    'use strict'; //satisfy code inspectors
-    var link = $(this); //preselect the link
-    if (link.hasClass('hover')) {
-        return true;
-    } else {
-        link.addClass('hover');
-        $('#dropdown-main').not(this).removeClass('hover');
-        e.preventDefault();
-        return false; //extra, and to make sure the function has consistent return points
-    }
+	return false;
 });
